fd32c5879eaefa7a944e80c08802f004fcd51c01
Andreas Bruncrona <andreas.bruncrona@helsinki.fi>
2022-03-26 15:18:20 +0200

    ensimmäinen testi

--- laskarit/viikko2/maksukortti/src/tests/maksukortti_test.py
+++ laskarit/viikko2/maksukortti/src/tests/maksukortti_test.py
@@ -7,3 +7,10 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_hello_world(self):
         self.assertEqual("Hello world", "Hello world")
+
+    def test_konstruktori_asettaa_saldon_oikein(self):
+        kortti = Maksukortti(10)
+
+       	vastaus = str(kortti)
+
+	self.assertEqual(vastaus, "Kortilla on rahaa 10 euroa")
