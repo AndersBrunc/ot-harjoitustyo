0b25693ff37b98bd7f079c358b2f9b4cb026b89d
Andreas Bruncrona <andreas.bruncrona@helsinki.fi>
2022-04-02 18:44:10 +0300

    users.py changes

--- budgetapp/src/entities/user.py
+++ budgetapp/src/entities/user.py
@@ -1,1 +1,26 @@
 class User:
+   
+   # Class that represents the user
+
+   # Attributes:
+   #    username: string , represents the users username
+   #    password: string , represents the users password
+   #    balance: float , represents the users current balance
+   #    income: float , represents the users current monthly income
+   #    expenses: float , represents the users monthly recurring expenses
+   
+   
+   def __init__(self,username=str,password=str,balance=float,income=float,expenses=float):
+        
+   #    Args:
+   #    username: string , represents the users username
+   #    password: string , represents the users password
+   #    balance: float , represents the users current balance
+   #    income: float , represents the users current monthly income
+   #    expenses: float , represents the users monthly recurring expenses
+       
+        self.username = username
+        self.password = password
+        self.balance = balance
+        self.income = income
+        self.expenses = expenses
