01d371393c5b6b1c5fe11c19d34156a30411b4c7
Andreas Bruncrona <andreas.bruncrona@helsinki.fi>
2022-04-02 19:19:54 +0300

    modified budget.py and user.py

--- budgetapp/src/entities/budget.py
+++ budgetapp/src/entities/budget.py
@@ -1,2 +1,28 @@
 class Budget:
- 
+
+    '''Class that describes a budget
+    
+    Attributes:
+        user: User-object, represents the user
+        name: string, represents name of the budget
+        original_amount: float, represents the original budget amount 
+        left_amount: float, represents the amount that is still left in the budget
+
+    '''
+
+    def __init__(self,user,name=str,orignal_amount=float):
+       
+        '''Class construcor, makes a new budget.
+    
+        Args:
+            user: User-object, represents the user
+            name: string, represents name of the budget
+            original_amount: float, represents the original budget amount 
+            left_amount: float, represents the amount that is still left in the budget
+
+        '''
+       
+        self.user=user
+        self.name=name
+        self.original_amount=original_amount
+        self.left_amount=original_amount
--- budgetapp/src/entities/user.py
+++ budgetapp/src/entities/user.py
@@ -1,23 +1,25 @@
 class User:
-   
-   # Class that represents the user
+   '''Class that represents the user
 
-   # Attributes:
-   #    username: string , represents the users username
-   #    password: string , represents the users password
-   #    balance: float , represents the users current balance
-   #    income: float , represents the users current monthly income
-   #    expenses: float , represents the users monthly recurring expenses
-   
-   
-   def __init__(self,username=str,password=str,balance=float,income=float,expenses=float):
-        
-   #    Args:
-   #    username: string , represents the users username
-   #    password: string , represents the users password
-   #    balance: float , represents the users current balance
-   #    income: float , represents the users current monthly income
-   #    expenses: float , represents the users monthly recurring expenses
+    Attributes:
+        username: string , represents the users username
+        password: string , represents the users password
+        balance: float , represents the users current balance
+        income: float , represents the users current monthly income
+        expenses: float , represents the users monthly recurring expenses
+    '''
+    
+    def __init__(self,username=str,password=str,balance=float,income=float,expenses=float):
+        '''Class constructor, makes new user.
+
+        Args:
+            username: string , represents the users username
+            password: string , represents the users password
+            balance: float , represents the users current balance
+            income: float , represents the users current monthly income
+            expenses: float , represents the users monthly recurring expenses
+
+        '''
        
         self.username = username
         self.password = password
