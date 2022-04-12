70912c1de95335ef5f51902699c141899ae12520
Andreas Bruncrona <andreas.bruncrona@helsinki.fi>
2022-04-12 22:34:27 +0300

    update gitignore and stuf

--- budgetapp/src/config.py
+++ budgetapp/src/config.py
@@ -3,10 +3,16 @@ from dotenv import load_dotenv
 
 directory = os.path.dirname(__file__)
 
-BUDGET_FILENAME = os.getenv('BUDGET_FILENAME') or budgets.csv
+try:
+    load_dotenv(dotenv_path=os.path.join(directory, '..', '.env'))
+except FileNotFoundError:
+    pass
+
+
+BUDGET_FILENAME = os.getenv('BUDGET_FILENAME')
 BUDGET_FILE_PATH = os.path.join(directory, '..', 'data', BUDGET_FILENAME)
 
-PURCHASE_FILENAME = os.getenv('PURCHASE_FILENAME') or purchases.csv
+PURCHASE_FILENAME = os.getenv('PURCHASE_FILENAME')
 PURCHASE_FILE_PATH = os.path.join(directory, '..', 'data', PURCHASE_FILENAME)
 
 DATABASE_FILENAME = os.getenv('DATABASE_FILENAME') or 'database.sqlite'
